908963d51b3bb771a3cb2f5dae47c727544cc452
Büşra Nur Bahadır <48088166+bsrnrbhdr@users.noreply.github.com>
2020-05-28 15:15:59 +0300

    Update UserDetailsServiceImp.java

--- src/main/java/com/MNA/MNA/Session/UserDetailsServiceImp.java
+++ src/main/java/com/MNA/MNA/Session/UserDetailsServiceImp.java
@@ -30,30 +30,4 @@ public class UserDetailsServiceImp implements UserDetailsService {
         return builder.build();
     }
 
-//    private UserRepository userRepository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        User user = userRepository.findByUsername(username);
-//        UserBuilder builder = null;
-//        if (user != null) {
-//            builder = org.springframework.security.core.userdetails.User.withUsername(username);
-//            builder.password(new BCryptPasswordEncoder().encode(user.getPassword()));
-//            builder.roles("USER");
-//        } else {
-//            throw new UsernameNotFoundException("User not found.");
-//        }
-//
-//        return builder.build();
-//    }
-//
-//    private User findUserbyUsername(String username) {
-//        User user= new User();
-//        if(username.equalsIgnoreCase("admin")) {
-//            user.setUsername(username);
-//            user.setPassword("admin123");
-//            return user;
-//        }
-//        return null;
-//    }
 }
